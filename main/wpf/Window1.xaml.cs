29fa7d6334ddbe701464b4a80a92b1c8cfc20b95
NathanHowell <nathan.d.howell@gmail.com>
2007-11-03 16:29:48 +0000

    The UI now shows linked zones properly, still a lot of muck in the command binding
    git-svn-id: https://sonority.svn.codeplex.com/svn@13496 5ff7c347-ad56-4c35-b696-ccb81de16e03

--- main/wpf/Window1.xaml.cs
+++ main/wpf/Window1.xaml.cs
@@ -115,69 +115,90 @@ namespace wpf
         void MediaCommands_TogglePlayPause(object sender, ExecutedRoutedEventArgs e)
         {
             TabControl tc = (TabControl)sender;
-            ZonePlayer zp = (ZonePlayer)tc.SelectedItem;
+            ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+            ZonePlayer zp = zg.Coordinator;
             zp.AVTransport.PlayPause();
+            e.Handled = true;
         }
 
         void MediaCommands_CanTogglePlayPause(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = true;
+            try
+            {
+                TabControl tc = (TabControl)sender;
+                ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+                ZonePlayer zp = zg.Coordinator;
+                e.CanExecute = zp.AVTransport.TransportState != TransportState.STOPPED;
+                e.Handled = true;
+            }
+            catch (NullReferenceException)
+            {
+                // wpf likes to render the button before the zoneplayer properties
+                // have filled in, 
+            }
         }
 
-        void MediaCommands_Next(object sender, ExecutedRoutedEventArgs e)
+        void MediaCommands_NextTrack(object sender, ExecutedRoutedEventArgs e)
         {
             TabControl tc = (TabControl)sender;
-            ZonePlayer zp = (ZonePlayer)tc.SelectedItem;
+            ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+            ZonePlayer zp = zg.Coordinator;
             zp.AVTransport.Next();
             e.Handled = true;
         }
 
-        void MediaCommands_CanNext(object sender, CanExecuteRoutedEventArgs e)
+        void MediaCommands_CanNextTrack(object sender, CanExecuteRoutedEventArgs e)
         {
             TabControl tc = (TabControl)sender;
-            ZonePlayer zp = (ZonePlayer)tc.SelectedItem;
+            ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+            ZonePlayer zp = zg.Coordinator;
             e.CanExecute = zp.AVTransport.CurrentTrack < zp.AVTransport.NumberOfTracks;
             e.Handled = true;
+        }
+
+        void MediaCommands_PreviousTrack(object sender, ExecutedRoutedEventArgs e)
+        {
+            TabControl tc = (TabControl)sender;
+            ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+            ZonePlayer zp = zg.Coordinator;
+            zp.AVTransport.Previous();
+            e.Handled = true;
+        }
+
+        void MediaCommands_CanPreviousTrack(object sender, CanExecuteRoutedEventArgs e)
+        {
+            TabControl tc = (TabControl)sender;
+            ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+            ZonePlayer zp = zg.Coordinator;
+            e.CanExecute = zp.AVTransport.CurrentTrack > 0;
+            e.Handled = true;
+        }
+
+        void MediaCommands_MuteVolume(object sender, ExecutedRoutedEventArgs e)
+        {
+            TabControl tc = (TabControl)sender;
+            ZoneGroup zg = (ZoneGroup)tc.SelectedItem;
+            ZonePlayer zp = zg.Coordinator;
+            zp.RenderingControl.SetMute(Channel.Master, !zp.RenderingControl.Mute[Channel.Master]);
+            e.Handled = true;
+        }
+
+        void MediaCommands_CanMuteVolume(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
 
         void ZoneVolumeChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             ushort volume = (ushort)Math.Round(e.NewValue);
-            Slider s = e.Source as Slider;
-            ZonePlayer zp = s.DataContext as ZonePlayer;
+            Slider s = (Slider)e.Source;
+            ZonePlayer zp = (ZonePlayer)s.DataContext;
             s.Delay = 100;
             if (volume != zp.RenderingControl.Volume[Channel.Master])
             {
                 zp.RenderingControl.SetVolume(Channel.Master, volume);
             }
             e.Handled = true;
-        }
-
-        void ProgressBar_Initialized(object sender, EventArgs e)
-        {
-            ProgressBar pb = sender as ProgressBar;
-            ZonePlayer zp = pb.DataContext as ZonePlayer;
-            zp.AVTransport.PropertyChanged += new PropertyChangedEventHandler(AVTransport_PropertyChanged);
-            _avpb[zp.AVTransport] = pb;
-        }
-
-        Dictionary<AVTransport, ProgressBar> _avpb = new Dictionary<AVTransport, ProgressBar>();
-
-        void AVTransport_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName != "TransportState")
-            {
-                return;
-            }
-
-            AVTransport av = sender as AVTransport;
-            bool enable = av.TransportState == TransportState.PLAYING;
-            ProgressBar pb = _avpb[av];
-
-            pb.Dispatcher.BeginInvoke(DispatcherPriority.DataBind, (ThreadStart)delegate
-            {
-                pb.IsEnabled = enable;
-            });
         }
 
         private Discover _discover = new Discover();
